a5a0fb775a4bc3281a3655e5d07efd7dc03969d8
Mary Snegova <Marya0@yandex.ru>
2022-10-31 21:15:34 +0300

    add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <Header /> {/* шапка */}
+        <Navigation /> {/* навигация */}
+        <Switch>
+        <Route exact path="/">{/* О проекте */}
+          <Main />
+        </Route>
+        <Route path="/movies">{/* Фильмы */}
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">{/* Сохранённые фильмы */}
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">{/* Аккаунт */}
+          <Profile />
+        </Route>
+        <Route path="/signin">{/* Авторизация */}
+          <Login />
+        </Route>
+        <Route path="/signup">{/* Регистрация */}
+          <Register />
+        </Route>
+        </Switch>
+        <Footer />{/* Подвал */}
+      </div>
     </div>
   );
 }
